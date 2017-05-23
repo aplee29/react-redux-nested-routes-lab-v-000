bb6f1595dd1d18673abe87262181094df4367dc1
Ann Lee <aplee29@users.noreply.github.com>
2017-05-22 21:35:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
 
+import PetsPage from './containers/PetsPage';
+import PetsNew from './containers/PetsNew';
+import PetsShow from './containers/PetsShow';
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App} >
+        <Route path="/pets" component={PetsPage} >
+          <Route path="/pets/new" component={PetsNew} />
+          <Route path="/pets/:id" component={PetsShow} />
+        </Route>
       </Route>
     </Router>
   </Provider>,
-  document.getElementById('root'))
+  document.getElementById('root')
+);
